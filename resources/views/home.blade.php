44ee2569329155e09220d83fd137df96dfffcdd8
Fgonzalez-js <fgonzalezpodesta@gmail.com>
2020-03-11 15:43:13 -0300

    Agrego Home, css y ruta

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/stylesHome.css">
+    <title>Home</title>
+</head>
+<body>
+    <h1>
+        "Bienvenidos al home"
+    </h1>
+</body>
+</html>
